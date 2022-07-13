e7129b8607c04aafd7fb7ddd4e6b6d7040654fab
Alex Ameen <alex.ameen.tx@gmail.com>
2022-07-13 18:42:44 -0500

    output module docs

--- flake.nix
+++ flake.nix
@@ -1,9 +1,15 @@
 {
   description = "Node.js Package Registry server";
-  inputs.utils.url = "github:numtide/flake-utils";
-  inputs.utils.inputs.nixpkgs.follows = "nixpkgs";
 
-  outputs = { self, nixpkgs, utils }: utils.lib.eachDefaultSystem ( system: let
+  inputs.utils.url  = "github:numtide/flake-utils";
+  inputs.ak-nix.url = "github:aakropotkin/ak-nix";
+  inputs.ak-nix.inputs.nixpkgs.follows = "/nixpkgs";
+  inputs.ak-nix.inputs.utils.follows   = "/utils";
+  inputs.utils.inputs.nixpkgs.follows  = "/nixpkgs";
+
+  outputs = { self, nixpkgs, utils, ak-nix }: let
+    inherit (utils.lib) eachDefaultSystem;
+  in eachDefaultSystem ( system: let
     pkgsFor = nixpkgs.legacyPackages.${system};
     nodePackages = import ./default.nix { pkgs = pkgsFor; };
     inherit (nodePackages) sources shell nodeDependencies verdaccio;
@@ -13,9 +19,21 @@
       pkgs = pkgsFor;
       verdaccioUnwrapped = nodePackages.verdaccio;
     };
+    verdaccioDocs = ( self.evalVerdaccioConfig {
+      inherit system;
+      pkgs = pkgsFor;
+      verdaccioUnwrapped = nodePackages.verdaccio;
+      withDocs = true;
+    } ).docs;
     app = utils.lib.mkApp { drv = verdaccio; exePath = "/bin/verdaccio"; };
   in {
-    packages = { default = verdaccio; inherit verdaccio verdaccioWrapped; };
+    packages = {
+      default = verdaccio;
+      inherit verdaccio verdaccioWrapped;
+      configDocs-all = verdaccioDocs.all;
+      configDocs-html = verdaccioDocs.html;
+      configDocs-markdown = verdaccioDocs.markdown;
+    };
     defaultPackage = verdaccio;
 
     nodeShell   = shell;
@@ -29,7 +47,9 @@
     };
     defaultApp = app;
 
-    overlays = final: prev: { inherit verdaccio verdaccioWrapped; };
+    overlays = final: prev: {
+      inherit verdaccio verdaccioWrapped verdaccioDocs;
+    };
   } ) // {
 
     # Used to evaluate the module as if it were a normal function.
@@ -42,6 +62,8 @@
     , pkgs               ? nixpkgs.legacyPackages.${system}
     , lib                ? nixpkgs.lib
     , verdaccioUnwrapped ? self.packages.${system}.verdaccio
+    , withDocs           ? false
+    , docgen             ? import "${ak-nix}/pkgs/docgen" { inherit pkgs nixpkgs; }
     }: let
       em  = lib.evalModules {
         modules = [
@@ -52,12 +74,14 @@
               verdaccio = lib.recursiveUpdate {
                 enable = true;
                 wrapper.enable = true;
-              } config.verdaccio;
+              } ( config.verdaccio or {} );
             };
           }
         ];
       };
-    in em.config.verdaccio;
+      docs = docgen.generateDocsForOptions em.options;
+    in em.config.verdaccio // ( lib.optionalAttrs withDocs { inherit docs; } );
+
     mkVerdaccioWrapper = args:
       ( self.evalVerdaccioConfig args ).wrapper.package;
 
